350c6b5baecfdc8a6abbeb689ebf70188fb6b782
user <>
2019-07-19 21:02:57 +0800

    重新思考

--- security/src/main/java/com/setty/security/access/intercept/aspectj/AspectJCallback.java
+++
@@ -1,9 +0,0 @@
-package com.setty.security.access.intercept.aspectj;
-
-/**
- * @author HuSen
- * create on 2019/7/18 18:42
- */
-public interface AspectJCallback {
-    Object proceedWithObject();
-}
